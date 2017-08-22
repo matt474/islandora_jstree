2567a84bc0915b9d4868b01414956a57a0172abb
Matt Bujold <matt474@live.ca>
2017-08-22 13:27:46 -0300

    Improved error and warning messages

--- js/error.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  Drupal.behaviors.islandoraJSTree = {
-    attach: function(context, settings) {
-      var div = document.getElementById('jstree-error');
-      div.innerHTML = 'jstree is unable to read the zip file.';
-    }
-  };
-})(jQuery);
--- theme/islandora-jstree.tpl.php
+++ theme/islandora-jstree.tpl.php
@@ -1,3 +1,11 @@
+<?php if(isset($errorMessage)): ?>
+  <?php print "<p class=\"jstree-error\">$errorMessage</p>" ?>
+  <?php endif ?>
+
+<?php if(isset($truncated)): ?>
+  <?php print "<p class=\"jstree-warning\">$truncated items have been truncated.</p>" ?>
+  <?php endif ?>
+
 <div id="jstree">
-  <p id="jstree-error" class="jstree-error"></p>
+  
 </div>
